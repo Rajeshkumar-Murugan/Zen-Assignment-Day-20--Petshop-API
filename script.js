3cfb809a99495ca911269a38863a94410b103255
Rajeshkumar-Murugan <rajeshkumarmurugan1@gmail.com>
2023-01-22 07:27:55 +0530

    Top Button

--- script.js
+++ script.js
@@ -138,3 +138,22 @@ var getdetails =  Promise.all([
    randomfact()
   
   //-------Display random fact function ends -----------
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
